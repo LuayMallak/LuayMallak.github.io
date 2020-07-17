2624a935dcb745690f224fe34b04351e14f10404
Luay Mallak <luay.mallak@gmail.com>
2020-07-17 18:44:01 +0200

    hhtp instead of https

---
+++ build/precache-manifest.d5f84b5587ab758004d8b4bfd7aab80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8169dfae9f4245a10128ba9746f57465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08928cbe98c40ab965ab",
+    "url": "/static/css/main.c21400cc.chunk.css"
+  },
+  {
+    "revision": "460bf84cf2b8a1114e4c",
+    "url": "/static/js/2.fcd87cad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcd87cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08928cbe98c40ab965ab",
+    "url": "/static/js/main.74e89b4a.chunk.js"
+  },
+  {
+    "revision": "e805431567c9c818cd19",
+    "url": "/static/js/runtime-main.78b94065.js"
+  }
+]);
