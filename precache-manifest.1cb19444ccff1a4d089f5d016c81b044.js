fcc221c2166b304811f6b7d78028b17d2843aa54
Luay Mallak <luay.mallak@gmail.com>
2020-08-11 11:36:21 +0200

    Updates

--- precache-manifest.1cb19444ccff1a4d089f5d016c81b044.js
+++ precache-manifest.1cb19444ccff1a4d089f5d016c81b044.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1d8f4e76c8da6cdf932555e30c8cb3e",
+    "revision": "15726029b7d62baa280e5d1c38c07a1b",
     "url": "/index.html"
   },
   {
-    "revision": "ffc52a1969921fe20ac3",
-    "url": "/static/css/main.4d1238bc.chunk.css"
+    "revision": "6ce37906104d7a4ee675",
+    "url": "/static/css/main.fffbfa90.chunk.css"
   },
   {
-    "revision": "44bf70ab601ce1afbe00",
-    "url": "/static/js/2.3d3ba111.chunk.js"
+    "revision": "f84cc16273b6e2ccc0b3",
+    "url": "/static/js/2.df34bf3c.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.3d3ba111.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.df34bf3c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ffc52a1969921fe20ac3",
-    "url": "/static/js/main.90cc9213.chunk.js"
+    "revision": "6ce37906104d7a4ee675",
+    "url": "/static/js/main.60678093.chunk.js"
   },
   {
     "revision": "e805431567c9c818cd19",
