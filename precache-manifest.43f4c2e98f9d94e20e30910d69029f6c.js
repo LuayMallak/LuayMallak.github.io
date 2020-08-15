a3b8caf50c0787b56930151068ce397286b3cc12
Luay Mallak <luay.mallak@gmail.com>
2020-08-15 15:50:53 +0200

    Updates

--- precache-manifest.43f4c2e98f9d94e20e30910d69029f6c.js
+++ precache-manifest.43f4c2e98f9d94e20e30910d69029f6c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ceadd7028aaf6e02cdf781ce7fbda6e",
+    "revision": "8bf36ac27439839be7560852a1655818",
     "url": "/index.html"
   },
   {
-    "revision": "c39bebcdbbaa00b684e6",
-    "url": "/static/css/main.61f902d7.chunk.css"
+    "revision": "2e617d0112ffb2a6fc40",
+    "url": "/static/css/main.5c094c0f.chunk.css"
   },
   {
     "revision": "f13c0cf4d62a880d4aed",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a6ff2b1c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c39bebcdbbaa00b684e6",
-    "url": "/static/js/main.759ccf5d.chunk.js"
+    "revision": "2e617d0112ffb2a6fc40",
+    "url": "/static/js/main.1d0f3c36.chunk.js"
   },
   {
     "revision": "e805431567c9c818cd19",
