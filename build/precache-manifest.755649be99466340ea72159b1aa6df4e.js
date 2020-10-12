ad5aec9ef7af8af92e5751d76b402c070e051f5f
Luay Mallak <luay.mallak@gmail.com>
2020-10-12 16:17:29 +0200

    adjusting the cards pictures

---
+++ build/precache-manifest.755649be99466340ea72159b1aa6df4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98952d2a2b628c8244434ec0127d1de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2f5f6f3f082f5f2edfe",
+    "url": "/static/css/main.bb7fb90e.chunk.css"
+  },
+  {
+    "revision": "798665d6dbb006362ee3",
+    "url": "/static/js/2.75866f68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75866f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f5f6f3f082f5f2edfe",
+    "url": "/static/js/main.b322ade8.chunk.js"
+  },
+  {
+    "revision": "e805431567c9c818cd19",
+    "url": "/static/js/runtime-main.78b94065.js"
+  },
+  {
+    "revision": "4dc6e0ea28359fba95bdc1f79fb315f3",
+    "url": "/static/media/2.4dc6e0ea.JPG"
+  },
+  {
+    "revision": "e941df1dfdc0493e074b606e8a4d606c",
+    "url": "/static/media/video2.e941df1d.mp4"
+  }
+]);
