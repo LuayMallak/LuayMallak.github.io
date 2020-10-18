d531730884d707a8003fc31d1f89c8b0e7a78e13
Luay Mallak <luay.mallak@gmail.com>
2020-10-18 15:10:54 +0200

    Updates

---
+++ precache-manifest.aea06c2295420ff2e25899997402db42.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f034cc9071f4f903a0da34a2d6d08d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a641cf5f4c5854b5977",
+    "url": "/static/css/main.3bab00ad.chunk.css"
+  },
+  {
+    "revision": "798665d6dbb006362ee3",
+    "url": "/static/js/2.75866f68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75866f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a641cf5f4c5854b5977",
+    "url": "/static/js/main.eb9f8eb9.chunk.js"
+  },
+  {
+    "revision": "e805431567c9c818cd19",
+    "url": "/static/js/runtime-main.78b94065.js"
+  },
+  {
+    "revision": "4dc6e0ea28359fba95bdc1f79fb315f3",
+    "url": "/static/media/2.4dc6e0ea.JPG"
+  },
+  {
+    "revision": "abb228db853629544139e00b47aeb8ed",
+    "url": "/static/media/html-css-portfolio.abb228db.png"
+  },
+  {
+    "revision": "7c5d6c94635ad91f1cf63196f38b6e25",
+    "url": "/static/media/movie-app.7c5d6c94.png"
+  },
+  {
+    "revision": "50f04e29f0d525a1636efbfc7cf9d72b",
+    "url": "/static/media/react-cocktail-master.50f04e29.png"
+  },
+  {
+    "revision": "9cfc1b948c3e60071d4d19862f05f8bb",
+    "url": "/static/media/todo-api-github.9cfc1b94.png"
+  },
+  {
+    "revision": "a99749f20b0be72c4564992c6f785f02",
+    "url": "/static/media/vanilla-javascript-cocktail-master.a99749f2.png"
+  },
+  {
+    "revision": "e941df1dfdc0493e074b606e8a4d606c",
+    "url": "/static/media/video2.e941df1d.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.755649be99466340ea72159b1aa6df4e.js"
+  "/precache-manifest.aea06c2295420ff2e25899997402db42.js"
 );
 
 self.addEventListener('message', (event) => {
